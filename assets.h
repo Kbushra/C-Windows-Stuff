cff2486782551bb81bc02b5fd38eddc990080022
kbushra <aaqibchoudhury3@gmail.com>
2026-05-14 18:58:45 +0100

    Didnt commit deletions

--- assets.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdint.h>
-#include <windows.h>
-#include "code_object.h"
--- property.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-typedef struct property property;
-struct property
-{
-    char* name;
-    void* value;
-};
